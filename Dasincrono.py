fa9c2f1e3e5227615ba84152c5ff252b93826faf
carlospuigserver <cpuiggut@myuax.com>
2023-02-21 16:41:43 +0100

    widget y descargar

--- Dasincrono.py
+++ Dasincrono.py
@@ -14,3 +14,23 @@ async def main(uri):
                 return await response.read()  
   
 asyncio.run(main("http://www.formation-python.com/")) 
+
+
+
+async def wget(session, uri):  
+    async with session.get(uri) as response:  
+        if response.status != 200:  
+            return None  
+        if response.content_type.startswith("text/"):  
+            return await response.text()  
+        else:  
+            return await response.read() 
+
+
+async def download(session, uri):  
+    content = await wget(session, uri)  
+    if content is None:  
+        return None  
+    with open(uri.split(sep)[-1], "wb") as f:  
+        f.write(content)  
+        return uri 
